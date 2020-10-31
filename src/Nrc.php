497c09aa08fedb4053e9d2474a43c4e8620e14f0
Toe Tet Aung Myint <toetet248@gmail.com>
2020-10-31 20:56:30 +0630

    checking

--- src/Nrc.php
+++ src/Nrc.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Request;
 
 class Nrc {
 
+    protected function getStringBetween($content, $start, $end){
+        $r = explode($start, $content);
+        if (isset($r[1])){
+            $r = explode($end, $r[1]);
+            return $r[0];
+        }
+        return '';
+    }
+    
     public function input()
     {
         return view('vendor.myanmarnrc.input');
@@ -56,14 +65,5 @@ class Nrc {
     {
         $arr = explode(')', $nrc);
         return $arr[1];
-    }
-
-    protected function getStringBetween($content, $start, $end){
-        $r = explode($start, $content);
-        if (isset($r[1])){
-            $r = explode($end, $r[1]);
-            return $r[0];
-        }
-        return '';
     }
 }
